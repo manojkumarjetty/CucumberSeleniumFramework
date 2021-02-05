40e2bca111432360d2641a6636bfdb1e210012f1
91990 <91990@192.168.0.104>
2021-02-05 11:28:18 +0530

    This contains web elements of Amazon Home Page.

---
+++ src/main/java/pageObjects/AmazonHomePage.java
@@ -0,0 +1,32 @@
+package pageObjects;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.CacheLookup;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class AmazonHomePage {
+	WebDriver driver;
+	 
+	 public AmazonHomePage(WebDriver driver) {
+	 this.driver = driver;
+	 PageFactory.initElements(driver, this);
+	 }
+	 
+	 @FindBy(id="twotabsearchtextbox")
+	 @CacheLookup
+	 public WebElement twotabsearchtextbox;
+	 
+	 @FindBy(id="nav-search-submit-button")
+	 @CacheLookup
+	 public WebElement searchsubmitbutton;
+	 
+	 public void clickSearchTextBox(String searchItem) {
+		 twotabsearchtextbox.sendKeys(searchItem);
+		}
+	 
+	 public void clickSearchSubmitButton() {
+		 searchsubmitbutton.click();
+		}
+}
