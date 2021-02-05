a629875706350c1e92279be1910d1631743d6247
91990 <91990@192.168.0.104>
2021-02-05 11:34:59 +0530

    This page contains Amazon search page objects.

--- src/main/java/pageObjects/AmazonHomePage.java
+++ src/main/java/pageObjects/AmazonHomePage.java
@@ -8,25 +8,26 @@ import org.openqa.selenium.support.PageFactory;
 
 public class AmazonHomePage {
 	WebDriver driver;
-	 
-	 public AmazonHomePage(WebDriver driver) {
-	 this.driver = driver;
-	 PageFactory.initElements(driver, this);
-	 }
-	 
-	 @FindBy(id="twotabsearchtextbox")
-	 @CacheLookup
-	 public WebElement twotabsearchtextbox;
-	 
-	 @FindBy(id="nav-search-submit-button")
-	 @CacheLookup
-	 public WebElement searchsubmitbutton;
-	 
-	 public void clickSearchTextBox(String searchItem) {
-		 twotabsearchtextbox.sendKeys(searchItem);
-		}
-	 
-	 public void clickSearchSubmitButton() {
-		 searchsubmitbutton.click();
-		}
+
+	public AmazonHomePage(WebDriver driver) {
+		this.driver = driver;
+		PageFactory.initElements(driver, this);
+	}
+
+	@FindBy(id = "twotabsearchtextbox")
+	@CacheLookup
+	public WebElement twotabsearchtextbox;
+
+	@FindBy(id = "nav-search-submit-button")
+	@CacheLookup
+	public WebElement searchsubmitbutton;
+
+	public void clickSearchTextBox(String searchItem) {
+		twotabsearchtextbox.sendKeys(searchItem);
+	}
+
+	public void clickSearchSubmitButton() {
+		searchsubmitbutton.click();
+	}
+
 }
